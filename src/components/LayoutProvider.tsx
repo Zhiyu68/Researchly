2f5adcc8876eb9c1626d51ad4a0daa9e3c04b864
zhiyuorbitx <zhiyu@orbitx.co.uk>
2023-11-21 19:31:29 +0000

    layout&side Navbar

--- src/components/LayoutProvider.tsx
+++ src/components/LayoutProvider.tsx
@@ -1,13 +1,108 @@
 "use client";
 import React from "react";
-import { Button, ConfigProvider, Space } from "antd";
+import { ConfigProvider } from "antd";
+import { usePathname } from "next/navigation";
 
 function LayoutProvider({ children }: { children: React.ReactNode }) {
+  const [isSidebarExpanded, setIsSidebarExpanded] = React.useState(true);
+  const menuitems = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-7-line",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "ri-shield-user-line",
+    },
+    {
+      name: "Applications",
+      path: "/applications",
+      icon: "ri-file-list-2-line",
+    },
+    {
+      name: "Settings",
+      path: "/settings",
+      icon: "ri-settings-2-line",
+    },
+    {
+      name: "Saved",
+      path: "/saved",
+      icon: "ri-save-line",
+    },
+  ];
+
+  const pathname = usePathname();
+
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <ConfigProvider theme={{ token: { colorPrimary: "#213555" } }}>
-          {children}
+          {/* if route is login or register , dont show layout */}
+
+          {pathname === "/login" || pathname === "/register" ? (
+            <div>{children}</div>
+          ) : (
+            <div className="layout-parent">
+              <div className="sidebar">
+                <div className="logo">
+                  {isSidebarExpanded && <h1>RESEARCHLY</h1>}
+
+                  {!isSidebarExpanded && (
+                    <i
+                      className="ri-menu-2-line"
+                      onClick={() => setIsSidebarExpanded(!isSidebarExpanded)}
+                    ></i>
+                  )}
+                  {isSidebarExpanded && (
+                    <i
+                      className="ri-close-line"
+                      onClick={() => setIsSidebarExpanded(!isSidebarExpanded)}
+                    ></i>
+                  )}
+                </div>
+
+                <div className="menu-items">
+                  {menuitems.map((item, index) => {
+                    const isActive = pathname === item.path;
+                    return (
+                      <div
+                        className={`menu-item ${
+                          isActive ? "active-menu-item" : ""
+                        }`}
+                        style={{
+                          justifyContent: isSidebarExpanded
+                            ? "flex-start"
+                            : "center",
+                        }}
+                      >
+                        <i className={item.icon}></i>
+                        <span>{isSidebarExpanded && item.name}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+
+                <div className="user-info">
+                  {isSidebarExpanded && (
+                    <div className="flex flex-col">
+                      <span>User Name</span>
+                      <span>User Email</span>
+                    </div>
+                  )}
+                  <i className="ri-logout-box-r-line"></i>
+                </div>
+              </div>
+              <div className="body">{children}</div>
+            </div>
+          )}
         </ConfigProvider>
       </body>
     </html>
